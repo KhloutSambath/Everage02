747d4fa627e7cb755e1ad41ccad07fe0e4b44ac5
KhloutSambath <65694832+KhloutSambath@users.noreply.github.com>
2021-07-22 10:36:35 +0700

    Presentation

---
+++ data/slide3.js
@@ -0,0 +1,6 @@
+(function(){var loadHandler=window['sl_{9376620A-D639-4BDD-A67A-A84F3332CB38}'];loadHandler&&loadHandler(2, '<div id="spr0_5206311"><div id="spr1_5206311" class="kern slide"><img id="img1_5206311" src="data/img1.png" width="960px" height="540px" alt="" style="left:0px;top:0px;"/></div><div id="spr2_5206311" class="kern slide"><div id="spr3_5206311" style="left:28px;top:39px;"><img id="img0_5206311" src="data/img4.png" width="833" height="283" alt="មធ្យម   18+12+30+26+38+42+48 7 =30.5≈30\
+តើជាមធ្យមក្នុងមួយថ្ងៃមានភ្ញៀវចូលកាត់សក់ចំនួនប៉ុន្មាននាក់? \
+ជាមធ្យមមានភ្ញៀវចូលកាត់សក់ចំនួន 30 នាក់ ក្នុងមួយថ្ងៃ ។ \
+ដើម្បីរកមធ្យមគេត្រូវធ្វើដូចម្តេច? \
+ដើម្បីរកមធ្យមគេត្រូវបូកចំនួនភ្ញៀវសរុប  ហើយចែកនឹងចំនួនថ្ងៃសរុប ។\
+								"/></div></div></div>');})();
